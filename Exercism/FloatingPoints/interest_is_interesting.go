8e517ade64a6558d849f534862e496c60cfa9acd
hambyhacks <hambyhaxx@gmail.com>
2021-12-03 21:06:11 +0800

    Day 23 of 100: Solved Floating Point exercise.

---
+++ Exercism/FloatingPoints/interest_is_interesting.go
@@ -0,0 +1,39 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance < 1000:
+		return 0.5
+	case balance >= 1000 && balance < 5000:
+		return 1.621
+	case balance >= 5000:
+		return 2.475
+	}
+	panic("Please implement the InterestRate function")
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return float64(InterestRate(balance)) * (balance / 100)
+	panic("Please implement the Interest function")
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return Interest(balance) + balance
+	panic("Please implement the AnnualBalanceUpdate function")
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	years := 0
+	for balance < targetBalance {
+		balance = AnnualBalanceUpdate(balance)
+		years++
+	}
+	return years
+	panic("Please implement the YearsBeforeDesiredBalance function")
+}
