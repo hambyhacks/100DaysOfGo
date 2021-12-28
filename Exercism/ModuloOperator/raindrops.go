c8749514449b9db741bb9d7189f7255f87e0c68c
hambyhacks <hambyhaxx@gmail.com>
2021-12-28 08:44:09 +0800

    Day 47 of 100: Finished Modulo Operator Exercise at Exercism.

---
+++ Exercism/ModuloOperator/raindrops.go
@@ -0,0 +1,34 @@
+package raindrops
+
+import "strconv"
+
+func Convert(number int) string {
+	if number%3 == 0 && number%5 == 0 && number%7 == 0 {
+		return "PlingPlangPlong"
+	}
+
+	if number%3 == 0 && number%5 == 0 {
+		return "PlingPlang"
+	}
+
+	if number%5 == 0 && number%7 == 0 {
+		return "PlangPlong"
+	}
+
+	if number%3 == 0 && number%7 == 0 {
+		return "PlingPlong"
+	}
+
+	if number%3 == 0 {
+		return "Pling"
+	}
+
+	if number%5 == 0 {
+		return "Plang"
+	}
+
+	if number%7 == 0 {
+		return "Plong"
+	}
+	return strconv.Itoa(number)
+}
